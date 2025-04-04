c214ad2b7ee84e5539d5a918396689ec35e4e30b
ivens magno <ivensmagno@gmail.com>
2025-04-04 17:07:50 -0300

    Incrimento do Swagger na classe principal

--- src/main/java/clinica/medica/MedicaApplication.java
+++ src/main/java/clinica/medica/MedicaApplication.java
@@ -1,11 +1,16 @@
 package clinica.medica;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Clinica Medica",
+		version = "1.0",
+		description = "Documentação das APIs do projeto Clinica Medica"))
 public class MedicaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MedicaApplication.class, args);
 	}
