f558491979b4181fb4b33ffa26919f215b4955ab
ivens magno <ivensmagno@gmail.com>
2025-04-10 00:56:21 -0300

    melhoria nos DTO

--- src/main/java/clinica/medica/dtos/MedicosDto.java
+++ src/main/java/clinica/medica/dtos/MedicosDto.java
@@ -1,19 +1,36 @@
 package clinica.medica.dtos;
 
 import clinica.medica.Enum.EspecialidadeEnum;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record MedicosDto(
+        @NotBlank
         String name,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
         String telefone,
+        @NotBlank
         String cpf,
         String endereco,
         String numero,
         String complemento,
         String bairro,
+        @NotBlank
         String cidade,
+        @NotBlank
         String uf,
+        @Pattern(regexp = "\\d{8}")
+        @NotBlank
         String cep,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String crm,
+        @NotNull
         EspecialidadeEnum especialidade) {
 }
--- src/main/java/clinica/medica/dtos/PacientesDto.java
+++ src/main/java/clinica/medica/dtos/PacientesDto.java
@@ -1,16 +1,23 @@
 package clinica.medica.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record PacientesDto(
+        @NotBlank
         String name,
+        @NotBlank
         String email,
+        @NotBlank
         String telefone,
+        @NotBlank
         String cpf,
+        @NotBlank
         String endereco,
         String numero,
         String complemento,
         String bairro,
         String cidade,
+        @NotBlank
         String uf,
-        String cep,
-        String crm) {
+        String cep) {
 }
